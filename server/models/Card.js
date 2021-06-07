0d73eec2103325b8ec85437c4c101dfb603259de
Ahmed Belhadj <mr.ahmedbelhadj@gmail.com>
2021-06-07 16:56:54 -0500

    Update Card.js
    include tags, color code, deadline, attachment

--- server/models/Card.js
+++ server/models/Card.js
@@ -1,14 +1,25 @@
 const mongoose = require("mongoose");
 
 const cardSchema = new mongoose.Schema({
-  // Cards need only to have a name and description. No comments, deadline, attachments, etc
+  // Cards have a name and description by default.
   name: {
     type: String,
     required: true,
   },
   description: {
     type: String,
+    default: "",
   },
+  // Tags, color code, deadline, attachments are only stored when added.
+  tags: [{ type: String }],
+  colorCode: {
+    type: String,
+  },
+  deadline: {
+    type: Date,
+  },
+  attachments: [{ type: String }],
+  // No comments yet.
 });
 
 module.exports = mongoose.model("card", cardSchema);
