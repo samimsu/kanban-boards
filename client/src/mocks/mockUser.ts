9f4a0bdb462de7c70508e6725a3e53493fd5c701
Ahmed Belhadj <mr.ahmedbelhadj@gmail.com>
2021-06-07 23:06:11 -0500

    Update mockUser.ts
    add `profilePicture` because of changes to User interface

--- client/src/mocks/mockUser.ts
+++ client/src/mocks/mockUser.ts
@@ -3,19 +3,23 @@ import { User } from '../interface/User';
 const mockLoggedInUser: User = {
   email: 'mockLoggedInUser@gmail.com',
   username: 'mock LoggedIn user',
+  profilePicture: 'https://robohash.org/mockLoggedInUser@gmail.com.png',
 };
 
 const mockOtherUser1: User = {
   username: 'Mock test user 1',
   email: 'mockTestUser1@gmail.com',
+  profilePicture: 'https://robohash.org/mockTestUser1@gmail.com.png',
 };
 const mockOtherUser2: User = {
   username: 'Mock test user 2',
   email: 'mockTestUser2@gmail.com',
+  profilePicture: 'https://robohash.org/mockTestUser2@gmail.com.png',
 };
 const mockOtherUser3: User = {
   username: 'Mock test user 3',
   email: 'mockTestUser3@gmail.com',
+  profilePicture: 'https://robohash.org/mockTestUser3@gmail.com.png',
 };
 
 const mockOtherUsers: User[] = [mockOtherUser1, mockOtherUser2, mockOtherUser3];
