9a0d6e03bad1b62c3bd090c1c3fa374cfad1c6b1
samimsu <mohammadsamimsu@gmail.com>
2021-06-09 15:08:10 +0400

    removed commented out code

--- client/src/components/BoardUI/AddCardButton.tsx
+++ client/src/components/BoardUI/AddCardButton.tsx
@@ -3,11 +3,6 @@ import useStyles from './useStyles';
 import Button from '@material-ui/core/Button';
 import CreateCardDialog from '../CreateCardDialog/CreateCardDialog';
 
-// interface Props {
-//   // createBoardOpen: boolean;
-//   // handleCloseCreateBoard: () => void;
-// }
-
 const AddCardButton = (): JSX.Element => {
   const classes = useStyles();
 
